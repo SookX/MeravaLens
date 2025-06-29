1fa5dbda1b800fe5ed9b5505c52927f7e437f491
MartiiPartii <125606563+MartiiPartii@users.noreply.github.com>
2025-06-29 23:13:31 +0300

    Map Integration

--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -1,28 +1,71 @@
-import { useContext, useEffect } from "react"
+import { useContext, useEffect, useState } from "react"
 import { DataContext } from "../../context/DataContext"
 import { Typography } from "@mui/material"
+import Map from "./components/Map/Map"
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 
 const Dashboard = () => {
     const { crud } = useContext(DataContext)
 
 
-
+    // Holds the selected coordinates
+    const [coords, setCoords] = useState(null)
+    
+    
+    
     useEffect(() => {
         const fetching = async () => {
             const response = await crud({
-                url: "/environment/?lat=42.6977&lon=23.3219",
+                url: `/environment/?lat=${coords.lat}&lon=${coords.lng}`,
                 method: "get"
             })
 
             console.log(response)
         }
-        fetching()
-    }, [])
+
+        if(coords) {
+            console.log(coords)
+            fetching()
+        }
+    }, [coords])
 
 
+    const key = import.meta.env.VITE_GOOGLE_API_KEY
+
+    // Stores map settings
+    const mapStyles = {
+        height: "100vh",
+        width: "100%",
+        osition: "absolute",
+        top: 0,
+        left: 0
+
+    };
+    const defaultCenter = {
+        lat: 42.698334, // Latitude of default location (Sofia)
+        lng: 23.319941 // Longitude of default location
+    };
+
+    const handleSelectCoords = (e) => {
+        setCoords({ lat: e.latLng.lat(), lng: e.latLng.lng() })
+    }
 
     return (
-        <Typography variant="h1">Hi</Typography>
+        <>
+            <LoadScript googleMapsApiKey={key}>
+                <GoogleMap
+                    mapContainerStyle={mapStyles}
+                    zoom={15}
+                    center={defaultCenter}
+                    onClick={(e) => handleSelectCoords(e)}
+                >
+                    {
+                        coords &&
+                        <Marker position={coords} />
+                    }
+                </GoogleMap>
+            </LoadScript>
+        </>
     )
 }
 
---
+++ client/src/pages/Dashboard/components/Map/Map.jsx
@@ -0,0 +1,16 @@
+
+import { useEffect, useState } from "react"
+
+const Map = ({ setCoords }) => {
+    
+    
+    
+    
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default Map
