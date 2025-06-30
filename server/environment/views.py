fff13e54f2f8c2b45f8ce093e161e60179793436
anastassow <127148449+anastassow@users.noreply.github.com>
2025-06-30 15:19:24 +0300

    jwt auth

--- server/environment/views.py
+++ server/environment/views.py
@@ -1,10 +1,12 @@
 import requests
 from django.conf import settings
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated;
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def environmental_data(request):
     api_key = settings.OPENWEATHER_API_KEY
     if not api_key:
