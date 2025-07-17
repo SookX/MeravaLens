06562c33c5b9027fa3b9bbf60ca2f4bb6d6d82c6
MartiiPartii <125606563+MartiiPartii@users.noreply.github.com>
2025-07-17 14:59:47 +0300

    Set server url as env variable

--- client/src/api/crud.js
+++ client/src/api/crud.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'http://127.0.0.1:8000/api'
+axios.defaults.baseURL = import.meta.env.VITE_SERVER_BASE_URL
 
 // Makes a CRUD operation to the backend server
 export const crud = async ({ url, method, body = null, headers = null }) => {
