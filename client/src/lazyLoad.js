41cab4ccd39eca70bd1efd6139f19015d8ff58b9
MartiiPartii <125606563+MartiiPartii@users.noreply.github.com>
2025-08-09 23:47:51 +0300

    Try to fix

--- client/src/lazyLoad.js
+++ client/src/lazyLoad.js
@@ -3,7 +3,7 @@ import { lazy } from "react"
 export const lazyLoad = (path, namedExport) => {
     return lazy(() => {
         const promise = import(path)
-        if(namedExport == null) return promise
+        if(namedExport == null) return promise.then(module => ({ default: module.default }))
         else return promise.then(module => ({ default: module[namedExport] }))
     })
 }
